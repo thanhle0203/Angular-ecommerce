78a4ff115f7d9c4acea26711d47c7ccf76b4ab27
Thanh Le <thanhlevan0203@gmail.com>
2022-11-18 22:55:45 -0800

    populate product data

--- shopping-cart/src/app/models/product.ts
+++ shopping-cart/src/app/models/product.ts
@@ -4,4 +4,14 @@ export class Product {
   description: string;
   price: number;
   imageUrl: string;
+
+  constructor(id, name, description='', price=0, imageUrl=''){
+    this.id = id
+    this.name = name 
+    this.description = description
+    this.price = price 
+    this.imageUrl = imageUrl
+  }
+
+  
 }
--- shopping-cart/src/app/services/product.service.ts
+++ shopping-cart/src/app/services/product.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { Product } from '../models/product';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  products = [
-   
+  // TODO: Populate products from an API
+  products: Product[] = [
+   new Product(1, 'Product 1', 'This is product 1 description. The product is really cool', 100),
+   new Product(2, 'Product 2', 'This is product 1 description. The product is really cool', 150), 
+   new Product(1, 'Product 3', 'This is product 1 description. The product is really cool', 200),
+   new Product(2, 'Product 4', 'This is product 1 description. The product is really cool', 250), 
   ]
 
   constructor() { }
+
+  getProducts(): Product[] {
+    //TODO: Populate products from an API and return an Observation
+    return this.products
+  }
 }

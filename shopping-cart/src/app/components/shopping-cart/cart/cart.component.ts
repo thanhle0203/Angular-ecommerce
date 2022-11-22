941d3a7887cd9b210f2e98e7cca80f101e9da4ee
Thanh Le <thanhlevan0203@gmail.com>
2022-11-21 21:46:55 -0800

    Create list of product in Cart component

--- shopping-cart/src/app/components/shopping-cart/cart/cart.component.ts
+++ shopping-cart/src/app/components/shopping-cart/cart/cart.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartItems = [
+    { id: 1, productId: 1, productName: 'Test1', qty: 4, price: 100},
+    { id: 2, productId: 2, productName: 'Test2', qty: 5, price: 150},
+    { id: 3, productId: 3, productName: 'Test3', qty: 2, price: 120},
+    { id: 4, productId: 4, productName: 'Test4', qty: 3, price: 200},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {

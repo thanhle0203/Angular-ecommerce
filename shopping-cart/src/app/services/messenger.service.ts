86b305bd13692b559125f537eb34f669a4713dce
Thanh Le <thanhlevan0203@gmail.com>
2022-11-25 15:21:02 -0800

    created sendMsg and getMsg method

--- shopping-cart/src/app/services/messenger.service.ts
+++ shopping-cart/src/app/services/messenger.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessengerService {
+  
+  subject = new Subject()
 
   constructor() { }
 
-  sendMsg() {
-    
+  sendMsg(product) {
+    console.log(product)
+    this.subject.next(product) // Triggering an event 
   }
 
   getMsg() {
-
+    return this.subject.asObservable()
   }
 
 

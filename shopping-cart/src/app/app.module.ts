eb47d016abd36e6e298b9ceea1e6cfa1c505071d
Thanh Le <thanhlevan0203@gmail.com>
2022-11-26 00:48:26 -0800

    create products url db

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
@@ -12,6 +11,7 @@ import { ProductListComponent } from './components/shopping-cart/product-list/pr
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- shopping-cart/src/app/services/product.service.ts
+++ shopping-cart/src/app/services/product.service.ts
@@ -10,19 +10,6 @@ const apiUrl = 'http://localhost:3000/products';
 })
 export class ProductService {
 
-  // TODO: Populate products from an API
-  products: Product[] = [
-   new Product(1, 'Product 1', 'This is product description. The product is really cool', 100, 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/1200px-Scramble.svg.png'),
-   new Product(2, 'Product 2', 'This is product description. The product is really cool', 150, 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Rubik%27s_cube.svg/1966px-Rubik%27s_cube.svg.png'), 
-   new Product(3, 'Product 3', 'This is product description. The product is really cool', 200, 'https://i5.walmartimages.com/asr/ba72adea-2498-4a97-be12-a5592d30f413.781a19deb450c52bd9f92b32123ce2ab.jpeg'),
-   new Product(4, 'Product 4', 'This is product description. The product is really cool', 250, 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067.jpg'),
-   new Product(5, 'Product 5', 'This is product description. The product is really cool', 200, 'https://static.scientificamerican.com/sciam/cache/file/76CA6AF7-D83E-481F-B27BED55698AEB36_source.jpg'),
-   new Product(6, 'Product 6', 'This is product description. The product is really cool', 250, 'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2021/08/12/16287757674369.jpg'), 
-   new Product(7, 'Product 7', 'This is product description. The product is really cool', 200, 'https://static.scientificamerican.com/sciam/cache/file/76CA6AF7-D83E-481F-B27BED55698AEB36_source.jpg'),
-   new Product(8, 'Product 8', 'This is product description. The product is really cool', 250, 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067.jpg'),
-   new Product(9, 'Product 9', 'This is product description. The product is really cool', 200, 'https://static.scientificamerican.com/sciam/cache/file/76CA6AF7-D83E-481F-B27BED55698AEB36_source.jpg'),
-  ]
-
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
